e63544c3d0175a06ce3fa57201d00870a4fe3854
CristianMR87 <Cristian.morano@hotmail.com>
2024-11-18 22:56:53 +0100

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -137,7 +137,7 @@ const App: React.FC = () => {
     };
     
     return (
-        <div className="App p-5 mt-5 mx-auto max-w-screen-lg table-auto border-collapse border border-gray-200 rounded-xl bg-gradient-to-b from-[#75c3db] via-[#44a8a0] to-[#298a46] shadow-xl relative">
+        <div className="App p-5 mt-5 mx-96 table-auto border-collapse border border-gray-200 rounded-xl bg-gradient-to-b from-[#75c3db] via-[#44a8a0] to-[#298a46] shadow-xl relative">
             {/* Botón de ubicación en la esquina superior izquierda */}
             <button
                 onClick={handleCurrentLocation}
@@ -192,10 +192,10 @@ const App: React.FC = () => {
                 <div className="mt-4">
                     <div className="flex flex-col gap-4">
                         {/* Clima actual */}
-                        <div className="flex flex-col sm:flex-row sm:gap-4 gap-2">
-                            <div className="w-full sm:w-1/2 h-auto sm:h-[300px]">
-                                <h3 className="text-lg sm:text-xl font-bold mt-4">Current Weather</h3>
-                                <div className="mt-4 bg-gradient-to-b from-[#66a8bd] via-[#80c0b5] to-[#b1eed5] shadow-xl rounded-xl p-4 flex flex-wrap sm:flex-nowrap items-center justify-between h-full">
+                        <div className="flex gap-4 mb-4">
+                            <div className="flex flex-col w-1/2 h-[300px]">
+                                <h3 className="text-xl font-bold mt-4">Current Weather</h3>
+                                <div className="mt-4 bg-gradient-to-b from-[#66a8bd] via-[#80c0b5] to-[#b1eed5] shadow-xl rounded-xl p-4 flex items-center justify-between h-full">
                                     {/* Columna izquierda: Sunrise y Sunset */}
                                     <div className="flex flex-col items-start w-1/3">
                                         <div className="flex items-center gap-2">
@@ -301,7 +301,7 @@ const App: React.FC = () => {
 
                         {/* Pronóstico de los próximos 5 días */}
                         <h3 className="text-xl font-bold">Next 5 Days Forecast</h3>
-                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 mt-4">
+                        <div className="flex gap-4 mt-4">
                             {weatherData.daily_forecast.map((day, index) => (
                                 <div key={index} className="bg-gradient-to-b from-[#66a8bd] via-[#80c0b5] to-[#b1eed5] shadow-xl rounded-xl w-1/5 p-4 text-center">
                                     <p className="font-bold">
